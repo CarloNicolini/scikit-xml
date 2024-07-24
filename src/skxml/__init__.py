0de92a9513d0abe279b90dcfbb24d900f8a7f892
carlo <c.nicolini@ipazia.com>
2024-07-24 13:16:22 +0200

    added mean average precision

--- src/skxml/__init__.py
+++ src/skxml/__init__.py
@@ -1,4 +1,10 @@
-from _sk_metrics import precision_at_k, recall_at_k, ndcg_at_k, compute_metrics
+from _sk_metrics import (
+    precision_at_k,
+    recall_at_k,
+    ndcg_at_k,
+    compute_metrics,
+    mean_average_precision_at_k,
+)
 from _sk_scorers import (
     precision_at_k_scorer,
     recall_at_k_scorer,
